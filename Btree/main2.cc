1a9adbbd38af806f631b0b9af53a2701c07d6a46
thomas <thomas@LAPTOP-6KMLBAIH.localdomain>
2022-12-23 17:36:44 +0800

    fix-search

--- Btree/main2.cc
+++ Btree/main2.cc
@@ -7,11 +7,12 @@ using namespace std;
 int main(){
 	const int range = 30;
 	const int Max = pow(2, 30);
+	const int search_time = 100000;
 	auto start = chrono::steady_clock::now();
 	auto end = chrono::steady_clock::now();
 	ofstream fout;
 	fout.open("./Btreeout", ios_base::out);
-	for(int round = 0;round < 10;++round){
+	for(int round = 0;round < 5;++round){
 
 		for(int j = 10;j <= range;++j){
 			bool flag = 0;
@@ -27,7 +28,7 @@ int main(){
 			if(time > 900)
 				flag = true;
 			start = chrono::steady_clock::now();
-			for(int i = 0;i < times;++i)
+			for(int i = 0;i < search_time;++i)
 				btree.search(rand() % Max);
 			end = chrono::steady_clock::now();
 			time = (double)chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000000;
